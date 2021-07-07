a7bbbe46e650c6967e5c776813bb8a0934c492fc
oğulcan karayel <ogulcankarayel@ogulcan-MacBook-Pro.local>
2021-07-07 22:39:04 +0300

    add base service

---
+++ src/services/api.ts
@@ -0,0 +1,44 @@
+import { AxiosRequestConfig } from 'axios'
+import axiosInstance from 'services/client'
+import { IRequestConfig } from 'services/types'
+
+export default {
+  makeRequest: async <T>(
+    parameters: IRequestConfig,
+    endpoint: string
+  ): Promise<T> => {
+    return new Promise((resolve, reject) => {
+      const { method, params = {} } = parameters
+      const { REACT_APP_API_KEY } = process.env
+
+      const queryParams = getParams(params)
+
+      const config: AxiosRequestConfig = {
+        method,
+        url: `${endpoint}?key=${REACT_APP_API_KEY}${queryParams}`,
+      }
+
+      axiosInstance(config)
+        .then((response) => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+}
+
+const getParams = (params = {}) => {
+  let queryParams = ''
+
+  if (Object.keys(params).length > 0) {
+    queryParams = `&${Object.entries(params)
+      .map(([key, value]) => {
+        return `${key}=${value}`
+      })
+      .join('&')}`
+  }
+
+  return queryParams
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
 export * from 'types/routes'
+export * from 'types/common'
